306a7188089c00db87925831dd19393f794319ef
郭云亮 <1060950677@qq.com>
2020-10-12 10:58:40 +0800

    项目搭建，配置

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from  'react-dom';
+// import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+// 页面
+import Home from 'page/home/index.jsx';
+
+class App extends React.Component {
+  render() {
+    return
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
